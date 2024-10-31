1e37b81c935bb816b4e8b87f0f4e6a942204c9b1
PC <hoangtuan0796991061@gmail.com>
2024-10-31 19:11:02 +0700

    learning...

--- lims_app/urls.py
+++ lims_app/urls.py
@@ -19,5 +19,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home', views.home, name="home"),
+    path('', views.home, name="home"),
+    path('contact', views.contact, name="contact"),
+    path('save', views.save_student, name="save")
 ]
--- lims_app/views.py
+++ lims_app/views.py
@@ -8,3 +8,11 @@ from django.shortcuts import render,redirect
 def home(request):
     return render(request, "index.html", context={})
 
+def contact(request):
+    return render(request, "contact.html")
+
+def save_student(request):
+    #get student name entered by user
+    student_name = request.POST['student_name']
+    return render(request, "welcome.html",context={'student_name':student_name})
+    #context to send data to welcome.html file
